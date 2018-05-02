415057055f7d98bad234ed55162621a127325ff8
megane <kky001kky@yahoo.co.jp>
2018-05-02 01:40:42 +0000

    CRUD tasks contoller modified

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,10 +15,10 @@ class TasksController < ApplicationController
     @task = Task.new(task_params)
     
     if @task.save
-      flash[:success] = 'Task が正常に追加されました'
+      flash[:success] = 'タスクが正常に追加されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'Task が追加されませんでした'
+      flash.now[:danger] = 'タスクが追加されませんでした'
       render :new
     end
   end
@@ -31,10 +31,10 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
     
     if @task.update(task_params)
-      flash[:success] = 'Task が正常に更新されました'
+      flash[:success] = 'タスクが正常に更新されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'Task が更新されませんでした'
+      flash.now[:danger] = 'タスクが更新されませんでした'
       render :edit
     end
   end
@@ -43,7 +43,7 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
     @task.destroy
 
-    flash[:success] = 'Task は正常に削除されました'
+    flash[:success] = 'タスクは正常に削除されました'
     redirect_to tasks_url
   end
 end
